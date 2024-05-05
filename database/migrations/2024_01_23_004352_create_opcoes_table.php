91676f4fec84056887eaa80fcac950dd3a79f227
Tiago Souza <tiagobrr17@gmail.com>
2024-05-05 12:25:01 -0300

    gitignoe and table opcoes

---
+++ database/migrations/2024_01_23_004352_create_opcoes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('opcoes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',35);
+            $table->string('titulo',100);
+            $table->string('descricao',1200);
+            $table->string('imagem',255);
+            $table->float('preco',10,2);
+            // $table->boolean('por_pessoa')->default(false);
+
+            $table->unsignedBigInteger('comunidade_id')->unsigned();
+
+            $table->foreign("comunidade_id")
+            ->references("id")
+            ->on("comunidades")
+            ->onDelete("cascade");
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('opcoes');
+    }
+};
